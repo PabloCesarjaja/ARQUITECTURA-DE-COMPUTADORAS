06348cefbe6a09e5687c3968c1b905be067f5331
PabloCesarjaja <pablo.robledo0253@alumnos.udg.mx>
2025-08-18 16:17:35 -0600

    Add files via upload

---
+++ Participacion 01 - Compuerta AND/_and.v
@@ -0,0 +1,10 @@
+// Definir modulo y sus entradas y salidas
+module _and (input A, input B, output C);//guion bajo mayusculas o minusculas
+//2. declara señales/elementos internos
+//NA
+//3. comportamiento del modulo (asignaciones, instancias, conexiones)
+assign C = A&B;
+
+
+
+endmodule
